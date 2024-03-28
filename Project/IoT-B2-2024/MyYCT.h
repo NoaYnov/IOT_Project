7e38c8c2b2fdbfba14ff0944ac2f6e12268136d4
Dimitri Brancourt <dimitri.brancourt@gmail.com>
2024-03-28 18:27:54 +0100

    update the YCT base file

--- Project/IoT-B2-2024/MyYCT.h
+++ Project/IoT-B2-2024/MyYCT.h
@@ -11,49 +11,38 @@
 #include <ArduinoJson.h> //
 
 // Variables to use :
-// set up your variables (for exemple the parameters from Adafruit or the contact list)
-
-
-// NOTE : if we use setup and loops we must write again all the programs of bluetooth, wifi ...
-// And deactivate the base setups and loops
-// There is still the possiblité to call these programs in their respective loops (BLE, WIFI, Adafruit)
-
-// ------------------------------------------------------------------------------------------------
-// Setup of our application
-// ------------------------------------------------------------------------------------------------
-void setupYCT(){
-  // TODO : simple setup
-}
-
-// ------------------------------------------------------------------------------------------------
-// Loop of our application
-// ------------------------------------------------------------------------------------------------
-void loopYCT(){
-  // TODO : simple loop 
-  // (for example, you can get nearby contacts, update your contacts and update your state)
-  // All of the programs could also be called by one program (getNearbyContact) it is as you see fit ...
-}
+// set up your variables (for exemple the parameters from Adafruit or the contact/positive list)
 
 // ------------------------------------------------------------------------------------------------
 // Main Functions for our application
 // ------------------------------------------------------------------------------------------------
 
 
-void getNearbyContacts(){
-  // TODO : Implementing the function that gets the informations of nearby cards (with the right format)
-  // This function should use the bluetooth and the Adafruit parameters in order to save the cards
-  // WARNING : this function should be in two parts, saving the cards and adding contact to follow
-  // if we were next to a cards during a fixed duration (parameters in Adafruit)
+void onGetContact(){
+  // TODO : Implementing the function that saves the informations of nearby cards (with the right format)
+  // WARNING : this function should call the serialize json function and update a contact as
+  // a contact to follow after a certain time
+  // This function should be in the bluetooth loop and be called when a card is found
+}
+
+void onUpdateState(){
+  // TODO : Implementing the function that Updates your state from Adafruit and your contacts
+  // You should use the deserialize function
+  // This function should be called in the adafruit loop when the state is changed in the app
 }
 
 void updateState(){
-  // TODO : Implementing the function that Updates your state from Adafruit and your contacts
+  // TODO : Implementing the function that Updates your state from your contacts
   // if one of your contact to follow is sick, you should be changed to contact case
+  // So you should verify if you have the same person in the contact list and positive list
+  // You should use the deserialize function
+  // This function should be called in the adafruit loop
 }
 
 void updateContacts(){
   // TODO : Implementing the function that Updates your contacts
   // the goal of this function is to update the informations of yours contacts to follow
+  // This function should be called in the wifi loop
 }
 
 // There should be a admin part, where you could setup OTA and Remote Debug,
